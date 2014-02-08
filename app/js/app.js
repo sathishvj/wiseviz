8525e10aca2a2a7417877c42b808852a42a54f60
sathishvj <sathishvj@yahoo.com>
2014-02-08 14:20:29 +0530

    landing and about page shows up on starting

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
+  $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
+  $routeProvider.otherwise({redirectTo: '/main'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,42 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+    .controller('NavCtrl', ["$scope", "$http", "$location", '$route', function ($scope, $http, $location, optionsService, $route ) {
+		switch($location.path()) {
+			case "":
+			case "/home":
+				$scope.activeLink = "Home";
+				break;
+			case "/about":
+				$scope.activeLink = "About";
+				break;
+			case "/admin":
+				$scope.activeLink = "Admin";
+				break;
+			case "/options":
+				$scope.activeLink = "Options";
+				break;
+			case "/feedback":
+				$scope.activeLink = "Feedback";
+				break;
+		}
+
+        $scope.setActive = function (selLink) {
+			//this call doesn't seem to stop the page from reloading when clicking an already selected link
+			if ($scope.activeLink === selLink) {
+				return;
+			}
+            $scope.activeLink = selLink;
+        };
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MainCtrl', [function() {
+
+  }])
+  .controller('GridCtrl', [function() {
+
+  }])
+  .controller('AboutCtrl', [function() {
 
   }]);
