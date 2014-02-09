b6357b37f4d8a59cbbe4b8e36c7bae5360336c9a
sathishvj <sathishvj@yahoo.com>
2014-02-09 08:43:12 +0530

    added viz boxes, data for some more charts, and one big chart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -49,7 +49,7 @@ angular.module('myApp.controllers', [])
   	}
 
   }])
-  .controller('GridCtrl', ['$scope', function($scope) {
+  .controller('GridVizCtrl', ['$scope', function($scope) {
 		$scope.gridOptions = {
 			data: 'gridData',
 		    columnDefs: 'gridCols'
@@ -57,8 +57,13 @@ angular.module('myApp.controllers', [])
 
 	  $scope.inputDataList = [
 		  {title: "Age-Population", file:"files/age-population.csv", cols: ["nominal", "number" ]},
-		  {title: "Apple Stock Price", file:"files/apple-stock-price.tsv", cols: ["ordinal/date/dd-MMM-yy", "number" ]},
-		  {title: "Temp Diff NY-SFO", file:"files/temp-diff-ny-sfo.tsv", cols: ["ordinal/date/yyyymmdd", "number", "number" ]}
+		  {title: "Apple Stock Price", file:"files/apple-stock-price.tsv", cols: ["ordinal", "number" ]},
+		  {title: "Browser Market Share", file:"files/browser-market-share.tsv", cols: ["nominal", "number", "number", "number", "number", "number" ]},
+		  {title: "Daily Average Temperatures", file:"files/daily-avg-temperatures.tsv", cols: ["ordinal", "number", "number", "number"]},
+		  {title: "Daily High-Low Temperatures", file:"files/daily-high-low-temp.tsv", cols: ["ordinal", "number", "number"]},
+		  {title: "Letter Frequency", file:"files/letter-frequency.tsv", cols: ["nominal", "number" ]},
+		  {title: "Population by State by Age Group", file:"files/populations-by-state.csv", cols: ["nominal", "number", "number", "number", "number", "number", "number", "number"]},
+		  {title: "Temp Diff NY-SFO", file:"files/temp-diff-ny-sfo.tsv", cols: ["ordinal", "number", "number" ]}
 		  ];
 
 	  $scope.chosenInputData = "";
@@ -70,26 +75,24 @@ angular.module('myApp.controllers', [])
 				  return;
 			  }
 			  console.log( "$watch: chosenFile changed.: ", newValue );
-			  $scope.updateGrid(newValue.file);
+			  $scope.updateGrid(newValue);
+		  	  $scope.updateVizList(newValue);
 		  });
 
-	  $scope.updateGrid = function(filename) {
+	  $scope.updateGrid = function(val) {
+		  var filename = val.file;
 		  var d3fn = d3.csv;
 		  if (filename.substr(-3) === "tsv") {
 			  console.log("It is a tsv file: ", filename);
 			  d3fn = d3.tsv;
 		  }
 
-		  //console.log("Function is: ", d3fn);
-
 		  d3fn(filename, function(inpData) {
 			  $scope.$apply(function(){
-				  //$scope.gridCols = ["a", "b", "c"];
-				  //$scope.gridCols = (new Date().getTime()%2==0)?[{field: 'new_name', displayName: 'New Name'}, {field:'new_age', displayName:'New Age'},{field:'pin', displayName:'Pin'}]:[{field: 'new_name', displayName: 'Name'}, {field:'new_age', displayName:'Age'},{field:'pin', displayName:'My Pin'}];
 
 				  var k=[],p;
 				  for (p in inpData[0]) if (Object.prototype.hasOwnProperty.call(inpData[0],p)) k.push(p);
-				  console.log("Keys: ", k);
+				  //console.log("Keys: ", k);
 				  var cols = [];
 				  for (var i=0; i<k.length; i++ ){
 					  cols.push({field: k[i], displayName: k[i]});
@@ -97,11 +100,77 @@ angular.module('myApp.controllers', [])
 				  $scope.gridCols = cols;
 
 				  $scope.gridData = inpData;
-				  console.log("Input data is: ", $scope.gridData);
+				  //console.log("Input data is: ", $scope.gridData);
 			  });
 		  });
+
 	  };
 
+	  $scope.vizList = [
+	  	{name: "area", img: "img/area-chart.png", active: false, cols: ["ordinal", "number"]},
+	  	{name: "bar", img: "img/bar-chart.png", active: false, cols: ["nominal", "number"]},
+	  	{name: "bivariate-area", img: "img/bivariate-area-chart.png", active: false, cols: ["ordinal", "number", "number"]},
+	  	{name: "donut", img: "img/donut-chart.png", active: false, cols: ["nominal", "number"]},
+	  	{name: "line", img: "img/line-chart.png", active: false, cols: ["ordinal", "number"]},
+	  	{name: "multiseries-line", img: "img/multiseries-line-chart.png", active: false, cols: ["ordinal", "number", "number", "number"]},
+	  	{name: "normalized-stacked-bar", img: "img/normalized-stacked-bar-chart.png", active: false, cols: ["nominal", "number", "number", "number", "number", "number", "number", "number"]},
+	  	{name: "pie", img: "img/pie-chart.png", active: false, cols: ["nominal", "number"]},
+	  	{name: "stacked-area", img: "img/stacked-area-chart.png", active: false, cols: ["nominal", "number", "number", "number", "number", "number"]},
+	  	{name: "stacked-bar", img: "img/stacked-bar-chart.png", active: false, cols: ["ordinal", "number", "number", "number", "number", "number"]}
+	  ];
+
+	  $scope.updateVizList = function(val) {
+		var dataCols = val.cols;
+		console.log("Input data cols list: ", dataCols);
+
+		for (var i=0; i<$scope.vizList.length; i++) {
+			var viz = $scope.vizList[i];
+			//first compare length
+			if (viz.cols.length != dataCols.length) {
+				viz.active = false;
+				continue;
+			}
+
+			var equal = true;
+			for (var j=0; j<dataCols.length; j++) {
+				//console.log("Comparing: ", dataCols[j], viz.cols[j]);
+				if (dataCols[j] != viz.cols[j]) {
+					equal = false;
+					break;
+				}
+			}
+
+			viz.active = equal?true:false;;
+		}
+	  }
+
+	  $scope.showBigChart = false;
+	  $scope.drawChart = function(name) {
+		  console.log("To draw chart: " + name);
+		  console.log("Chosen input data: ", $scope.chosenInputData);
+		  $scope.showBigChart = true;
+		  switch(name) {
+			  case "pie":
+				  drawPieChart($scope.chosenInputData.file);
+				  break;
+		  }
+	  }
+  }])
+  .controller('VizCtrl', ["$scope", function($scope) {
+	  /*
+	  $scope.vizList = [
+	  	{img: "img/area-chart.png", active: false},
+	  	{img: "img/bar-chart.png", active: false},
+	  	{img: "img/bivariate-area-chart.png", active: false},
+	  	{img: "img/donut-chart.png", active: false},
+	  	{img: "img/line-chart.png", active: false},
+	  	{img: "img/multiseries-line-chart.png", active: false},
+	  	{img: "img/normalized-stacked-bar-chart.png", active: false},
+	  	{img: "img/pie-chart.png", active: false},
+	  	{img: "img/stacked-area-chart.png", active: false},
+	  	{img: "img/stacked-bar-chart.png", active: false}
+	  ];
+	  */
   }])
   .controller('AboutCtrl', ["$scope", function($scope) {
 		$scope.current = "";
